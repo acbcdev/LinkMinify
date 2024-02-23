fb71758fa93eeb4ff239e20fe844a805f001a411
ACBC <camiloblandon69@gmail.com>
2024-02-22 20:07:58 -0500

    add Button a nd Input componenst

--- app-link-minify/src/app/page.tsx
+++ app-link-minify/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-	return <div>ass</div>;
+	return (
+		<main className="bg-zinc-700 grid place-content-center h-screen w-full">
+			<Input type="url" placeholder="Link" required />
+			<Input type="text" placeholder="hash" />
+			<Button type="submit">Create</Button>
+		</main>
+	);
 }
