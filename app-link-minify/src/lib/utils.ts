d20aa03c0272d0724abfd944f845fd2e356ae184
ACBC <acbc.dev@gmail.com>
2024-03-01 16:02:05 -0500

    Añadida validación de URL en asegurarHttp()

--- app-link-minify/src/lib/utils.ts
+++ app-link-minify/src/lib/utils.ts
@@ -4,25 +4,20 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export function asegurarHttp(url: string): string {
-  // Ampliamos la expresión regular para aceptar segmentos de ruta y parámetros más complejos.
+export function asegurarHttp(url: string): string | null {
   const regex =
     /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?([\?&\w\.-=]*)$/;
-  // Verificamos si la URL cumple con la expresión regular ampliada.
-  if (regex.test(url)) {
-    // Verificamos si la URL ya comienza con http:// o https://.
-    if (!/^https?:\/\//i.test(url)) {
-      // Si no comienza con http:// o https://, le añadimos http:// al inicio.
-      return `http://${url}`;
-    }
-    // Si la URL ya comienza con http:// o https://, la retornamos sin cambios.
-    return url;
+
+  // Si la entrada no es una URL válida, retornamos null.
+  if (!regex.test(url)) {
+    return null;
   }
-  // Dado que deseamos aceptar URLs complejas, asumimos que cualquier entrada es una URL intentando ser válida.
-  // Si no comienza con http:// o https://, le añadimos http:// al inicio.
+
+  // Si la URL no comienza con http:// o https://, le añadimos http:// al inicio.
   if (!/^https?:\/\//i.test(url)) {
     return `http://${url}`;
   }
+
   // Si la URL ya comienza con http:// o https://, la retornamos sin cambios.
   return url;
 }
