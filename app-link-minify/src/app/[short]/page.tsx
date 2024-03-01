b4b870342a7fb47e83fbfc5bac59bb726b1dbc47
ACBC <acbc.dev@gmail.com>
2024-03-01 16:01:18 -0500

    Actualizar función de redirección en página [short]

--- app-link-minify/src/app/[short]/page.tsx
+++ app-link-minify/src/app/[short]/page.tsx
@@ -1,16 +1,10 @@
 import { redirect } from "next/navigation";
 export default async function page({ params }: { params: { short: string } }) {
-	const rta = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}/api/urls/${params.short}`,
-		{
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		},
-	);
+  const rta = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/urls/${params.short}`
+  );
 
-	const url = await rta.json();
-	// console.log(url[0].url);
-	redirect(url[0].url);
+  const url = await rta.json();
+  // console.log(url[0].url);
+  redirect(url[0].url);
 }
