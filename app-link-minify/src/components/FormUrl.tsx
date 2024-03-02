ecb490046c71374d6d71697da6914c58ee070d98
ACBC <acbc.dev@gmail.com>
2024-03-01 19:37:57 -0500

    Añadir validación de URL inválida en FormUrl

--- app-link-minify/src/components/FormUrl.tsx
+++ app-link-minify/src/components/FormUrl.tsx
@@ -12,7 +12,10 @@ export default function FormUrl() {
   const { addLink, links } = useLinkStore();
   async function createNewShort() {
     const linkToShort = asegurarHttp(url);
-
+    if (linkToShort === null) {
+      toast.error("Invalid URL");
+      return;
+    }
     const rta = await fetch("/api/urls", {
       method: "POST",
       headers: {
@@ -38,25 +41,27 @@ export default function FormUrl() {
         }}
         className=" flex flex-col md:flex-row items-center justify-center w-full gap-2"
       >
-        <Input
-          type="text"
-          placeholder="Enter your link here"
-          required
-          value={url}
-          onChange={(e) => {
-            e.preventDefault;
-            setUrl(e.target.value);
-          }}
-          className="w-64 text-white animate-fade-in-right animate-delay-300 border-white focus-visible:ring-0 focus:ring-offset-transparent focus:bg-white hover:bg-white hover:text-black duration-200 focus:outline-none focus:shadow-2xl shadow-white focus:text-black bg-transparent rounded-3xl border-4 p-6"
-        />
-        <Button
-          type="submit"
-          className={cn(
-            "border-4 bg-white text-zinc-950 rounded-3xl px-5 hover:scale-125 duration-300 animate-duration-300 animate-fade-in-left animate-delay-300 py-5 hover:bg-white hover:text-black border-white"
-          )}
-        >
-          Shorten URL
-        </Button>
+        <section className="flex flex-col md:flex-row gap-2 animate-bounce-fade-in animate-delay-300 animate-duration-250 ">
+          <Input
+            type="text"
+            placeholder="Enter your link here"
+            required
+            value={url}
+            onChange={(e) => {
+              e.preventDefault;
+              setUrl(e.target.value);
+            }}
+            className="w-64 text-white  border-white focus-visible:ring-0 focus:ring-offset-transparent focus:bg-white hover:bg-white hover:text-black duration-200 focus:outline-none focus:shadow-2xl shadow-white focus:text-black bg-transparent rounded-3xl border-4 p-6"
+          />
+          <Button
+            type="submit"
+            className={cn(
+              "border-4 bg-white text-zinc-950 rounded-3xl px-5 hover:scale-125 duration-300  py-5 hover:bg-white hover:text-black border-white"
+            )}
+          >
+            Shorten URL
+          </Button>
+        </section>
       </form>
       <Toaster position="top-center" richColors />
     </>
