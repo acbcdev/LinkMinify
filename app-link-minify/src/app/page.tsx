dac53ef2eb5c5df788c791bd618e9f4e5ab70ff7
ACBC <acbc.dev@gmail.com>
2024-03-01 19:17:43 -0500

    Actualizar estilos de la barra de desplazamiento y el diseño de la página principal

--- app-link-minify/src/app/page.tsx
+++ app-link-minify/src/app/page.tsx
@@ -2,14 +2,24 @@ import FormUrl from "@/components/FormUrl";
 import UrlsCreated from "@/components/UrlsCreated";
 export default function Home() {
   return (
-    <main className="grid place-content-center h-screen gap-10">
-      <header>
-        <h1 className="text-white text-center text-8xl animate-fade-in animate-delay-250">
-          Link Minify
-        </h1>
-      </header>
-      <FormUrl />
-      <UrlsCreated />
-    </main>
+    <>
+      <main id="Create" className="grid place-content-center h-screen gap-10">
+        <header>
+          <h1 className="text-white text-center text-8xl animate-fade-in animate-delay-250">
+            Link Minify
+          </h1>
+        </header>
+        <FormUrl />
+      </main>
+      <section
+        id="Links"
+        className="w-full grid place-content-center h-auto mb-16 "
+      >
+        <UrlsCreated />
+      </section>
+      <footer className="h-11">
+        <p className="text-white text-center">Made by ACBC</p>
+      </footer>
+    </>
   );
 }
