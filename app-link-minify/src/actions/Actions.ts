a08bf67881a38b84419fa2b383620d9a80c866fa
acbc <acbc.dev@gmail.com>
2024-05-16 19:06:56 -0500

    Refactor Actions.ts and add GetUrl and CreateUrl functions

--- app-link-minify/src/actions/Actions.ts
+++ app-link-minify/src/actions/Actions.ts
@@ -1,19 +1,20 @@
 'use server'
 
 import { connectDB } from "@/lib/mongodb";
+import { randomNum } from "@/lib/utils";
 import Hash from "@/models/hash";
 import { nanoid } from "nanoid";
 
 export async function GetUrl(hash: string) {
   await connectDB();
   const result = await Hash.findOne({ code: hash });
-  return result.url;
+  return result.url.json();
 }
 export async function CreateUrl(url: string) {
   await connectDB();
   const result = await Hash.create({
     url: url,
-    code: nanoid(7),
+    code: nanoid(randomNum()),
   });
-  return result;
+  return JSON.stringify(result);
 }
