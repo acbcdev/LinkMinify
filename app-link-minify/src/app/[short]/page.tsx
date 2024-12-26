003f6cc15b1d298f492e7880cfe59a42d9d12906
ACBC <acbc.dev@gmail.com>
2024-12-26 09:34:49 -0500

    delete

--- app-link-minify/src/app/[short]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-import { GetUrl } from "@/actions/Actions";
-export default async function page({ params }: { params: { short: string } }) {
-	const url = await GetUrl(params.short);
-
-	permanentRedirect(url);
-}
--- app-link-minify/src/lib/mongodb.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/nextmongo";
-export async function connectDB() {
-  await mongoose.connect(MONGODB_URL);
-}
